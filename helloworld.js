335e84a5711f7eb41b58d02a944f518b01eaae6b
Codo192 <93085410+Codo192@users.noreply.github.com>
2022-01-29 18:50:20 -0500

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('Hello World\n')
+   response.end('my week 2 homework Assignment\n')
 }).listen(3333)
 
 // Console will print the message
